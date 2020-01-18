d735df1a8bfe8ed99a47fc365ff419458aba7e76
mark bilim <markbilim0@gmail.com>
2020-01-18 19:02:55 +0200

    Changes

--- auth.py
+++ auth.py
@@ -1,35 +1,41 @@
-login = input('Login: ')
-password = input('Password: ')
+def user_login():
+        login = input('Login: ')
+        password = input('Password: ')
+        return login, password
 
-try:
-    users = open('Users.txt')
-except (OSError, IOError):
-    users = open('Users.txt', 'w')
-    users.write('1. Mark qwe123\n2. Ivan zxc123\n3. Oleg qwe123')
+login, password = user_login()
+
+def user_update():
+    try:
+        users = open('Users.txt')
+    except (OSError, IOError):
+        users = open('Users.txt', 'w')
+        users.write('1. Mark qwe123\n2. Ivan zxc123\n3. Oleg qwe123')
+        users.close()
+        users = open('Users.txt')
+
+
+    content = []
+    for line in users:
+        if not line:
+            continue
+            number, name, *passw = line.split()
+            content.append('{} {}'.format(name, ' '.join(passw)))
+    content.append('{} {}'.format(login, password))
     users.close()
-    users = open('Users.txt')
-
-
-content = []
-for line in users:
-    if not line:
-        continue
-    number, name, *passw = line.split()
-    content.append('{} {}'.format(name, ' '.join(passw)))
-content.append('{} {}'.format(login, password))
-users.close()
 
 ####
 
-for line in content:
-    name, passw = line.split()
-    if login == name and passw == password:
-        print('OK')
-        exit()
-    elif login == name and passw != password:
-        print('Not OK')
-        exit()
+    for line in content:
+        name, passw = line.split()
+        if login == name and passw == password:
+            print('OK')
+            exit()
+        elif login == name and passw != password:
+            print('Not OK')
+            exit()
 
+user_update(user_login())
 ####
 
 def get_key(line):
