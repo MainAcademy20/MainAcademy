73f0251fa33c0be617ed2922ab004937c755dfaa
YuriiShutko <yurayurban72@gmail.com>
2020-01-18 22:32:16 +0200

    Added functions read_credentials_from_file and add_user

--- auth.py
+++ auth.py
@@ -1,25 +1,44 @@
+from Encrypt_String import read_from_file, write_to_file
+
+
 def ask_credentials():
     input_login = input('Login: ')
     input_password = input('Password: ')
     return input_login, input_password
 
-try:
-    users = open('Users.txt')
-except (OSError, IOError):
-    users = open('Users.txt', 'w')
-    users.write('1. Mark qwe123\n2. Ivan zxc123\n3. Oleg qwe123')
-    users.close()
-    users = open('Users.txt')
+
+login, password = ask_credentials()
 
 
-content = []
-for line in users:
-    if not line:
-        continue
-    number, name, *passw = line.split()
-    content.append('{} {}'.format(name, ' '.join(passw)))
-content.append('{} {}'.format(login, password))
-users.close()
+def read_credentials_from_file(file_name):
+    content = []
+    try:
+        credentials = open(file_name)
+    except (OSError, IOError):
+        write_to_file(file_name, '1. Mark qwe123\n2. Ivan zxc123\n3. Oleg qwe123')
+        credentials = open(file_name)
+    for line in credentials:
+        if not line:
+            continue
+        number, name, *passw = line.split()
+        content.append('{} {}'.format(name, ' '.join(passw)))
+    credentials.close()
+    return content
+
+
+def add_user(content):
+    new_content = content
+    new_content.append('{} {}'.format(login, password))
+    return new_content
+
+
+print(read_credentials_from_file('Users.txt'))
+
+
+def write_credentials_to_file():
+    write_to_file('Users.txt', text)
+    pass
+
 
 ####
 
