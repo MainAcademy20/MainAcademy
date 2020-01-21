68967597f5938396bb02e0a86e01f109b4ed1c94
natalineverova <neverova.natali@gmail.com>
2020-01-21 16:16:01 +0200

    functions added

--- auth.py
+++ auth.py
@@ -1,5 +1,10 @@
-login = input('Login: ')
-password = input('Password: ')
+def user_login():
+    login = input('Login: ')
+    password = input('Password: ')
+    return login, password
+
+
+login, password = user_login()
 
 try:
     users = open('Users.txt')
@@ -7,40 +12,46 @@ except (OSError, IOError):
     users = open('Users.txt', 'w')
     users.write('1. Mark qwe123\n2. Ivan zxc123\n3. Oleg qwe123')
     users.close()
-    users = open('Users.txt')
 
-
+with open('Users.txt', 'r+') as users:
+    users = users.readlines()
 content = []
-for line in users:
-    if not line:
-        continue
-    number, name, *passw = line.split()
-    content.append('{} {}'.format(name, ' '.join(passw)))
-content.append('{} {}'.format(login, password))
-users.close()
 
-####
 
-for line in content:
-    name, passw = line.split()
-    if login == name and passw == password:
-        print('OK')
-        exit()
-    elif login == name and passw != password:
-        print('Not OK')
-        exit()
+def format_line():
+    for line in users:
+        number, name, *passw = line.split()
+        content.append('{} {}'.format(name, ' '.join(passw)))
 
-####
+
+format_line()
+
+
+def check_line():
+    for line in content:
+        name, passw = line.split()
+        if name == login and password == passw:
+            return print('ok')
+        elif name == login and password != passw:
+            return print('not ok')
+    if name != login:
+        content.append('{} {}'.format(login, password))
+        print('added')
+
+
+check_line()
+
 
 def get_key(line):
     return line[0].lower()
 
+
 content.sort(key=get_key)
 text = ''
 
+
 for i, login in enumerate(content, start=1):
     text += '{}. {}\n'.format(i, login)
-
 
 users = open('Users.txt', 'w')
 users.write(text)
