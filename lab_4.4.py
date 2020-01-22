424c4508beed1455fb69b7153abe2eb6778ab2ec
SergiiiPe <you@example.com>
2020-01-22 23:28:15 +0200

    version23.01.2020

--- lab_4.4.py
+++ lab_4.4.py
@@ -1,10 +1,6 @@
 '''
-foo = [1, 2, 3, 4, 5]
-odd_foo = [f for f in foo if f % 2 == 1]
-'''
-
 odd_foo = lambda x, y, z, w, q: [f for f in (x, y, z, w, q) if f % 2 == 1]
-'''
 i=odd_foo(1,2,3,4,5)
 print(i)
 '''
+odd_foo = list(filter((lambda f: f % 2 == 1), (range (1, 6))))
