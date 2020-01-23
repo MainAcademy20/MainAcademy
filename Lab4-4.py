f8d8c93815bc3dcbca4b19a22f0556ecfcb9e0b8
Yuriy Shutko <yurayurban72@gmail.com>
2020-01-23 10:28:06 +0200

    Added Lab4-4

---
+++ Lab4-4.py
@@ -0,0 +1,16 @@
+foo = [1, 2, 3, 4, 5]
+
+# odd_foo = []
+#
+#
+# def isOdd(x):
+#     return x % 2 == 1
+#
+#
+# for f in foo:
+#     if isOdd(f):
+#         odd_foo.append(f)
+#     print(odd_foo)
+
+print([num for num in foo if num % 2 == 1])
+print(list(filter(lambda x: x % 2 == 1, foo)))
