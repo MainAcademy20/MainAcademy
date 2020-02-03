66804ada101b22ad14d196a891af3acb99750554
Anton1912 <g.goreslavskyi@.gmail.com>
2020-02-03 23:07:23 +0200

    message

--- LAB_4.6.py
+++ LAB_4.6.py
@@ -1,10 +1,9 @@
 var = ["one", "two", "three", "four", "five"]
-file1 = "config_data_file.txt"
 def open_write_data_file(func):
     def open_write(data):
-        f = open("config_data_file.txt", "r+")
-        func(f, data)
-        f.close()
+        file = open("config_data_file.txt", "r+")
+        func(file, data)
+        file.close()
     return open_write
 
 
@@ -12,11 +11,13 @@ def open_write_data_file(func):
 @open_write_data_file
 def write_config(file, string):
     if "Configuration file! Do not modify!" in file.read():
-        message = file.write("{},\n".format(string))
+        message = file.write("{}\n".format(string))
     else:
         message = file.write("Configuration file! Do not modify!\n" + "{}\n".format(string))
+    file = open("config_data_file.txt", "r")
+    for x in file:
+        print(x)
     return message
-
 
 write_config(var)
 
