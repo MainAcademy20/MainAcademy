82c885d0196a8153984534d722a9c9150c9dd40f
Anton1912 <g.goreslavskyi@.gmail.com>
2020-02-06 13:11:36 +0200

    "message"

--- LAB_4.6.py
+++ LAB_4.6.py
@@ -11,9 +11,11 @@ def open_write_data_file(func):
 @open_write_data_file
 def write_config(file, string):
     if "Configuration file! Do not modify!" in file.read():
+        print("I`ll add in the file:\n", string)
         message = file.write("{}\n".format(string))
     else:
         message = file.write("Configuration file! Do not modify!\n" + "{}\n".format(string))
+        print("I`ll add in the file:\n Configuration file! Do not modify!\n", string)
     file = open("config_data_file.txt", "r")
     for x in file:
         print(x)
