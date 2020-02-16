6fbdd01c0a65699f773b473594bcea8ee2ab713e
YuriiShutko <yurayurban72@gmail.com>
2020-02-16 13:59:16 +0200

    First commit

---
+++ Tic_tac_toe/view.py
@@ -0,0 +1,55 @@
+"""
+вывести на экран:
+    игровое поле
+    состояние игры
+    запросить ввод текущего игрока
+    если надо, сообщение об ошибке
+
+o . .
+. . x
+. . .
+
+x|o > q|w|e|a|s|d|z|x|c
+x|o победил!
+ничья!
+"""
+import os
+import sys
+
+
+def print_field(xostate):
+    for i in range(3):
+        row = xostate.get_row(i)
+        for cell in row:
+            if cell == 0:
+                print('.', end=' ')
+            if cell == 1:
+                print('x', end=' ')
+            if cell == 2:
+                print('o', end=' ')
+        print()
+
+
+def get_input(xostate):
+    if xostate.current_player == 1:
+        print('x > ', end='')
+    if xostate.current_player == 2:
+        print('o > ', end='')
+    return input()
+
+
+def print_gameover(game_result):
+    if game_result == 1:
+        print('x победил!')
+    if game_result == 2:
+        print('o победил!')
+    if game_result == 3:
+        print('ничья!')
+    input()
+
+
+def clear_screen():
+    if sys.platform in ['linux', 'darwin']:
+        os.system('clear')
+    if sys.platform == 'win32':
+        os.system('cls')
