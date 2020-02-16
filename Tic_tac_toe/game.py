cbf7565c599c79708db2fcad94a3c604ad21371c
YuriiShutko <yurayurban72@gmail.com>
2020-02-16 14:03:26 +0200

    Added function check_input

--- Tic_tac_toe/game.py
+++ Tic_tac_toe/game.py
@@ -24,6 +24,18 @@ def get_new_game_status(xostate):
     return 3
 
 
+def check_input(local_turn, state, keys):
+    view.clear_screen()
+    view.print_field(state)
+    if local_turn not in ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c']:
+        view.show_error('1')
+        return False
+    if state.field[keys[local_turn][0]][keys[local_turn][1]] in [1, 2]:
+        view.show_error('2')
+        return False
+    return True
+
+
 def game_loop():
     """
          TODO::* добавить проверки на корректность ввода
@@ -44,11 +56,12 @@ def game_loop():
         view.clear_screen()
         view.print_field(state)
         turn = view.get_input(state)
+        while not check_input(turn, state, key_to_pos):
+            turn = view.get_input(state)
         pos_row, pos_col = key_to_pos[turn]
         state.set_cell(pos_row, pos_col, state.current_player)
         state.switch_player()
         game_status = get_new_game_status(state)
-
     view.clear_screen()
     view.print_field(state)
     view.print_gameover(game_status)
