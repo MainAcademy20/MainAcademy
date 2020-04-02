43756d061b7daadda8f9753dd0ceef07ebea60b4
BilimMark <59971889+BilimMark@users.noreply.github.com>
2020-04-02 17:36:30 +0300

    Update design.py

--- design.py
+++ design.py
@@ -1,4 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import Qt
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -11,38 +14,36 @@ class Ui_MainWindow(object):
         self.listView.setGeometry(QtCore.QRect(290, 290, 821, 521))
         self.listView.setObjectName("listView")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(290, 50, 191, 21))
+        self.lineEdit.setGeometry(QtCore.QRect(290, 70, 191, 21))
         self.lineEdit.setObjectName("lineEdit")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(510, 50, 201, 21))
+        self.lineEdit_2.setGeometry(QtCore.QRect(510, 70, 201, 21))
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(290, 250, 93, 28))
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(290, 30, 71, 16))
+        self.label.setGeometry(QtCore.QRect(290, 50, 71, 16))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(510, 30, 71, 16))
+        self.label_2.setGeometry(QtCore.QRect(510, 50, 71, 16))
         self.label_2.setObjectName("label_2")
-        """self.model = QtWidgets.QFileSystemModel(self)
-        self.model.setRootPath(QtCore.QDir.currentPath())"""
         self.treeView = QtWidgets.QTreeView(self.centralwidget)
         self.treeView.setGeometry(QtCore.QRect(10, 10, 271, 791))
         self.treeView.setObjectName("treeView")
-        """self.treeView.setModel(self.model)
-        cur = self.model.index(QtCore.QDir.currentPath())
-        self.treeView.expand(cur)"""
         self.verticalSlider = QtWidgets.QSlider(self.centralwidget)
         self.verticalSlider.setGeometry(QtCore.QRect(990, 290, 22, 521))
         self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
         self.verticalSlider.setObjectName("verticalSlider")
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(290, 80, 191, 22))
+        self.comboBox.setGeometry(QtCore.QRect(290, 220, 191, 22))
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(290, 10, 93, 28))
+        self.pushButton_2.setObjectName("pushButton_2")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1016, 26))
@@ -64,7 +65,24 @@ class Ui_MainWindow(object):
         self.comboBox.setItemText(0, _translate("MainWindow", "Одобрен"))
         self.comboBox.setItemText(1, _translate("MainWindow", "Откленен"))
         self.comboBox.setItemText(2, _translate("MainWindow", "Принят на работу"))
+        self.pushButton_2.setText(_translate("MainWindow", "Импорт"))
 
+
+class OPEN_FILES(QtWidgets):
+
+    def __init__(self):
+        super().__init__()
+        self.pushButton_2.clicked.connect(self.open_file_path)
+        self.model = QFileSystemModel(self)
+        self.model.setRootPath(QDir.currentPath())
+
+    def open_file_path(self, signal):
+        file_path = self.model.filePath(signal)
+        with open(file_path) as f:
+            txt = f.read()
+        idx = self.listView.addTab(Qt.QTextEdit(), file_path)
+        self.listView.widget(idx).setPlainText(txt)
+        self.listView.setCurrentIndex(idx)
 
 if __name__ == "__main__":
     import sys
